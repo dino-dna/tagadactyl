924a9e66222af5401daf9f158be4d167cf997438
cdaringe <christopher.dieringer@gmail.com>
2017-02-26 14:44:47 -0800

    mini

--- src/index.js
+++ src/index.js
@@ -17,10 +17,7 @@ var tdac = {
   getConfigureGroupButtonHTML: function (group) {
     return '<img id="TESTGROUP" class="taga-configure" src="' + chrome.extension.getURL('gear.svg') + '" />'
   },
-  /**
-   * returns all <ul>s from newly created github.com 'Tag User' dropdowns
-   */
-  filterSuggestedUsers: function (mutations) {
+  getNewULNodes: function (mutations) {
     return mutations.reduce(function (collection, mutation) {
       return collection.concat(Array.prototype.filter.call(
         mutation.addedNodes,
@@ -36,15 +33,15 @@ var tdac = {
     var imgNode = li.children[1]
     this.bindClickConfigureGroupHandler(imgNode)
   },
-  listenForComments: function () {
-    var observer = new MutationObserver(this.processCommentBoxes.bind(this))
+  listenForGithubMentionDropdowns: function () {
+    var observer = new MutationObserver(this.processNewMentionDropdowns.bind(this))
     observer.observe(document, { attributes: true, attributeFilter: ['class', 'classList'], childList: true, subtree: true })
   },
-  processCommentBoxes: function (mutations) {
-    var suggestedUsersNodes = this.filterSuggestedUsers(mutations)
-    if (!suggestedUsersNodes.length) return
-    this.injectGroup(testGroups[0], suggestedUsersNodes[0])
+  processNewMentionDropdowns: function (mutations) {
+    var ulNodes = this.getNewULNodes(mutations)
+    if (!ulNodes.length) return
+    this.injectGroup(testGroups[0], ulNodes[0])
   }
 }
 
-tdac.listenForComments()
+tdac.listenForGithubMentionDropdowns()
