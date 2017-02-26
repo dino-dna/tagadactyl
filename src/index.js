207511e49fa8a6aa99304023bbe86a5e6b777207
cdaringe <christopher.dieringer@gmail.com>
2017-02-26 00:36:38 -0800

    chore(pkg): stage incomplete groups

--- src/index.js
+++ src/index.js
@@ -2,27 +2,68 @@
 
 var MutationObserver = window.MutationObserver
 
+// @TODO, remove post testing
+var testGroups = [
+  {
+    name: 'old-timerz',
+    key: 'github.com/dino-dna/tagadactyl',
+    global: false,
+    users: [
+      { login: 'dylancwood' },
+      { login: 'rkelly' }
+    ]
+  },
+  {
+    name: 'def',
+    key: 'github.com/dino-dna/NOTTDACTYL',
+    global: false,
+    users: [
+      { login: 'swashcap' },
+      { login: 'wa11-e' }
+    ]
+
+  }
+]
 var tdac = {
-  _cache: {},
+  _cache: {
+    collaborators: {}, // GH collaborators, as stated by GH service. e.g. { 'dino-dna/tagadactyl': [...] }
+    groups: [] // user specified groups, e.g. [{ key: 'dino-dna/tagadactyl', name: 'mygroup', users: [...], global: boolean }]
+  },
+  get currentProjectKey () { return window.location.hostname + '/' + this.owner + '/' + this.repo },
+  get owner () { return window.location.pathname.split('/')[1] },
+  get repo () { return window.location.pathname.split('/')[2] },
+
+  bindClickConfigureGroupHandler (group) {
+    var node = document.getElementById(this.mapGroupToGroupId(group))
+    node.addEventListener('click', function (evt) {
+      evt.stopImmediatePropagation()
+      evt.stopPropagation()
+      evt.preventDefault()
+      console.log('blah!')
+    })
+  },
+  createListNode: function (opts) {
+    var li = document.createElement('li')
+    li.classList.add('js-navigation-item')
+    li.setAttribute('data-value', opts.textInjectedOnSelect)
+    return li
+  },
   /**
    * @returns {Promise<Object[],Error>}
    */
   getCollaborators: function () {
-    if (this._cache.collaborators) {
-      return Promise.resolve(this._cache.collaborators)
+    if (this._cache.collaborators[this.currentProjectKey]) {
+      return Promise.resolve(this._cache.collaborators[this.currentProjectKey])
     }
-    var pieces = window.location.pathname.split('/')
-    var owner = pieces[1]
-    var repo = pieces[2]
     var headers = new Headers()
     return this.getGHAPIToken()
       .then(function (key) {
         headers.append('Authorization', 'token ' + key)
         return fetch(
-          'https://api.github.com/repos/' + owner + '/' + repo + '/collaborators',
+          'https://api.github.com/repos/' + this.owner + '/' + this.repo + '/collaborators',
           { headers: headers }
         )
-      })
+      }.bind(this))
       .then(function (response) { return Promise.all([response, response.json()]) })
       .then(function (responses) {
         var response = responses[0]
@@ -35,9 +76,23 @@ var tdac = {
         return body
       })
       .then(function (collaborators) {
-        this._cache.collaborators = collaborators
+        this._cache.collaborators[this.currentProjectKey] = collaborators
         return collaborators
       }.bind(this))
+  },
+  getConfigureGroupButtonHTML: function (group) {
+    return '<img id={id} class="taga-configure" src="{url}" />'.supplant({
+      id: this.mapGroupToGroupId(group),
+      url: chrome.extension.getURL('gear.svg')
+    })
+  },
+  getGroups: function () {
+    return new Promise(function (resolve, reject) {
+      chrome.storage.local.get('groups', function (options) {
+        // return resolve((options.groups || []).filter(this.shouldRenderGroup))
+        resolve(testGroups.filter(this.shouldRenderGroup))
+      }.bind(this))
+    }.bind(this))
   },
   getGHAPIToken: function () {
     return new Promise(function (resolve, reject) {
@@ -62,6 +117,15 @@ var tdac = {
         }))
     }, [])
   },
+  handleProcessCommentBoxesDataSourcingFail: function (err) {
+    if (err && err.code === 'ENOGHKEY') {
+      return chrome.runtime.sendMessage({ openOptionsPage: true })
+    }
+    throw err
+  },
+  injectAdmin: function () {
+    // add Add/edit group
+  },
   /**
    * Inject collaborators into @-tag list
    * @param {object} opts
@@ -72,21 +136,38 @@ var tdac = {
   injectCollaborators: function (opts) {
     var collaborators = opts.collaborators
     var suggestedUsersNodes = opts.suggestedUsersNodes
-    var serializedCollaborators = collaborators
-      .map(function (collab, ndx) {
-        return (ndx ? '@' : '') + collab.login
-      })
-      .join(' ')
-    suggestedUsersNodes.forEach(function appendCollaborators (node) {
-      var li = document.createElement('li')
-      li.classList.add('js-navigation-item')
-      li.setAttribute('data-value', serializedCollaborators)
-      li.innerHTML = 'Collaborators <small>' + collaborators.length + ' users</small>'
-      node.appendChild(li)
-    })
+    var serializedCollaborators = collaborators.map(this.mapCollaboratorToGHTag).join(' ')
+    return suggestedUsersNodes.forEach(function appendCollaborators (node) {
+      var tagNode = this.createListNode({ textInjectedOnSelect: serializedCollaborators })
+      tagNode.innerHTML = 'Collaborators <small>' + collaborators.length + ' users</small>'
+      node.appendChild(tagNode)
+    }.bind(this))
+  },
+  /**
+   * @param {object} opts {@see indexDataSources}
+   * @returns {unde}
+   */
+  injectGroups: function (opts) {
+    var groupSet = opts.groups.map(function appendGroupNode (group) {
+      var serializedGroup = group.users.map(this.mapCollaboratorToGHTag).join(' ')
+      var tagNode = this.createListNode({ textInjectedOnSelect: serializedGroup })
+      tagNode.innerHTML = [
+        'Group: ', group.name, ' <small>',
+        group.users.length, ' users</small>',
+        this.getConfigureGroupButtonHTML(group)
+      ].join('')
+      group.tagNode = tagNode
+      return group
+    }.bind(this))
+    opts.suggestedUsersNodes.forEach(function appendGroupInjectButton (node) {
+      groupSet.forEach(function (group) {
+        node.appendChild(group.tagNode)
+        this.bindClickConfigureGroupHandler(group)
+      }.bind(this))
+    }.bind(this))
   },
   listenForComments: function () {
-    var observer = new MutationObserver(this.processCommentBoxes.bind(this))
+    var observer = new MutationObserver(this.processCommentBoxes)
     observer.observe(
       document,
       {
@@ -97,23 +178,48 @@ var tdac = {
       }
     )
   },
+  mapCollaboratorToGHTag: function (collab, ndx) {
+    return (ndx ? '@' : '') + collab.login
+  },
+  mapGroupToGroupId (group) {
+    return 'group_' + group.key.replace(/\W/g, '')
+  },
   processCommentBoxes: function (mutations) {
     var suggestedUsersNodes = this.filterSuggestedUsers(mutations)
     if (!suggestedUsersNodes.length) return
-    return this.getCollaborators()
-      .then(function (collaborators) {
-        return this.injectCollaborators({
-          collaborators: collaborators,
-          suggestedUsersNodes: suggestedUsersNodes
-        })
-      }.bind(this))
-      .catch(function (err) {
-        if (err && err.code === 'ENOGHKEY') {
-          return chrome.runtime.sendMessage({ openOptionsPage: true })
-        }
-        throw err
+    this.injectAdmin()
+    var asyncSources = Promise.all([
+      this.getCollaborators(),
+      this.getGroups(),
+      suggestedUsersNodes
+    ])
+      .then(function indexDataSources (datas) {
+        return { collaborators: datas[0], groups: datas[1], suggestedUsersNodes: datas[2] }
       })
+    // note, the below are intentionally not chained so as to provide each .then
+    // full unhidered access to the datas
+    asyncSources.then(this.injectCollaborators)
+    asyncSources.then(this.injectGroups)
+    asyncSources.catch(this.handleProcessCommentBoxesDataSourcingFail)
+  },
+  shouldRenderGroup: function (group) {
+    return group.global || group.key === this.currentProjectKey
   }
+}
+
+// eager bind tdac members, s.t. `this` behaves more `class``like
+for (var key in tdac) if (typeof tdac[key] === 'function') tdac[key] = tdac[key].bind(tdac)
+
+// https://gist.github.com/pbroschwitz/3891293
+// that's right, i went there.  sue me.
+String.prototype.supplant = function (o) {
+  return this.replace(
+    /{([^{}]*)}/g,
+    function (a, b) {
+      var r = o[b];
+      return typeof r === 'string' || typeof r === 'number' ? r : a;
+    }
+  )
 }
 
 tdac.listenForComments()
